ca09f3cd15b18faf41aef33e83b9861e140d2de1
ForestWatchAI <140009213+ForestWatchAI@users.noreply.github.com>
2023-07-23 21:19:01 +0530

    added cross origin

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ import imghdr
 import ssl
 import smtplib
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = MongoClient(
     "mongodb+srv://forestwatchai:hackathon%4069@forestwatchai.kshtlwm.mongodb.net/")
